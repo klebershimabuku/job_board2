2cd018fda017ab4fe7a7bddf5fade22faf8f3a6a
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-04-14 19:39:44 -0300

    initial commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.3'
+gem 'jquery-rails'
+
+group :developement do 
+  gem 'mysql2'
+end
+
+group :production do
+  gem 'pg', '0.12.2'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobBoard2::Application.config.secret_token = '2ee58913986bb3aba3bb02d649e0258b45b9a18ee0de1ef1e1007461d28e2a1de8b973eba570519eea9bbda687a78b8264a53650af491c58683e01a7f31c2437'
