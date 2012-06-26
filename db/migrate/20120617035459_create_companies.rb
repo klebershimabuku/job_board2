d03360e78cb4e99dbc48fdab216e2c880a3b24e6
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-06-26 13:30:03 -0300

    change company address column to text

--- db/migrate/20120617035459_create_companies.rb
+++ db/migrate/20120617035459_create_companies.rb
@@ -2,7 +2,7 @@ class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
       t.string :name
-      t.string :address
+      t.text :address
       t.text :description
 
       t.timestamps
