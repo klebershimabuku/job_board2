40e9d7046b10e54f0daaf5bcb97550531ce8ab45
Kleber Shimabuku <klebershimabuku@gmail.com>
2012-07-28 23:31:54 -0300

    add company name to URL (SEO)

--- app/models/company.rb
+++ app/models/company.rb
@@ -14,5 +14,9 @@ class Company < ActiveRecord::Base
   attr_accessible :address, :description, :name, :prefecture_ids
   has_and_belongs_to_many :prefectures
   validates :name, :address, presence: true
+  
+  def to_param
+    "#{id}-#{name.downcase.gsub(/[^[:alnum:]]/,'-')}".gsub(/-{2,}/,'-')
+  end
 end
 
